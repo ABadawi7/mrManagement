a40abd5f55e2dcfbb5c52ceb750f063d8f3d40b4
Abdulrahman Badawi <abd07.badawi@gmail.com>
2026-07-27 23:37:38 +0200

    Initial Spring Boot project setup

---
+++ src/main/java/org/example/mrmanagement/MrManagementApplication.java
@@ -0,0 +1,13 @@
+package org.example.mrmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MrManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/mrmanagement/MrManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.mrmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MrManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
